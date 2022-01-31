9440a438c02f1740b15393de7bd700e35c649f7a
otaylor2023 <75337325+otaylor2023@users.noreply.github.com>
2022-01-31 15:08:08 -0500

    seeded dataset

--- TabularMay2021/evolve.py
+++ TabularMay2021/evolve.py
@@ -4,17 +4,18 @@ import numpy as np
 import os
 import neat
 
+seed = 1
 
 # process values in train and test files (pandas)
-dataframe = pd.read_csv("/Users/evanxie/Evan's Stuff/Shallow Mind/Network1/DataSet1/train.csv")
-test_df = pd.read_csv("/Users/evanxie/Evan's Stuff/Shallow Mind/Network1/DataSet1/test.csv")
+dataframe = pd.read_csv("DataSet1/train.csv")
+test_df = pd.read_csv("DataSet1/test.csv")
 
 # remove unnecessary column from dataframe
 dataframe.drop("id", axis = 1, inplace = True)
 test_df.drop("id", axis = 1, inplace = True)
 
 # randomly order rows
-dataframe = dataframe.iloc[np.random.permutation(len(dataframe))]
+dataframe.sample(n=250, random_state=1)
 dataframe.reset_index(inplace = True)
 dataframe.drop("index", axis = 1, inplace = True)
 
